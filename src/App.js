fe4e1a3ce54280e6f69f01b01809e91396fdd101
Rafal Czajkowski <rafalxczajkowski@hotmail.com>
2022-11-04 17:28:32 +0100

    css changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="content-grid">
+        <header className="app-header">Oszacowywacz kredytu</header>
+        <div className="variable-field">
+          <label>Waluta kredytu:</label>
+          <select className="input-field">
+            <option value="pln">PLN</option>
+            <option value="chf">CHF</option>
+            <option value="eur">EUR</option>
+          </select>
+        </div>
+        <div className="variable-field">
+          <label>Podaj kwotę kredytu w zł:</label>
+          <input type="text" className="input-field" />
+        </div>
+        <div className="variable-field">
+          <label>Podaj datę uruchomienia kredytu:</label>
+          <input type="date" className="input-field" />
+        </div>
+        <div className="variable-field">
+          <label>Podaj dzień płatności raty:</label>
+          <input type="text" className="input-field" />
+        </div>
+        <div className="variable-field">
+          <label>Szacowana suma spłaconych rat:</label>
+          <input type="text" className="input-field" />
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ todo.md
@@ -0,0 +1,1 @@
+git add . && git commit -m 'MESSAGE' && git push
